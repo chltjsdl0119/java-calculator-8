34842ca6e15fff7aaf4e9dc03dcf7baf33b6cdea
Seungwon-Choi <bestwondev@gmail.com>
2025-10-19 15:38:32 +0900

    feat: 최종 실행문 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig config = new AppConfig();
+        config.run();
     }
 }
